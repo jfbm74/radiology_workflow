857c2269fb4ad4eb4f2d5846b70f051499fd9c08
Juan Bustamante <jfbm74@gmail.com>
2024-01-13 09:41:48 -0500

    Fixing Opportunity Report view blade

--- resources/views/reports/orders/opportunity.blade.php
+++ resources/views/reports/orders/opportunity.blade.php
@@ -34,7 +34,7 @@
 
 @section('content')
 
-    <h3 class="text-center"> Reporte de Oportunidad de Atención</h3>
+    <h3 class="text-center"> Reporte de Oportunidad de Atención  </h3>
     <div class="section-body mt-3">
         <div class="container-fluid">
             <div class="row clearfix">
@@ -44,22 +44,6 @@
                             <form action="{{route('report.opportunity')}}" method="GET">
                                 @csrf
                                 <div class="row">
-
-                                    <div class="col-lg-2 col-md-3 col-sm-6">
-                                        <label><strong> Técnico</strong></label>
-                                        <div class="input-group">
-                                            <div class="form-group">
-                                                <select class="form-control show-tick"
-                                                        name="technician">
-                                                    <option></option>
-                                                    @foreach($staff as $user)
-                                                        <option value="{{$user->id}}">{{$user->name}}</option>
-                                                    @endforeach
-
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
                                     <div class="col-lg-2 col-md-3 col-sm-6">
                                         <label><strong> Fecha Inicial</strong></label>
                                         <div class="input-group">
@@ -145,66 +129,69 @@
         </div>
     </div>
     @isset($data)
-                    <div class="section-body mt-3">
-                        <div class="container-fluid">
-                            <div class="row clearfix">
-                                <div class="col-lg-12">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h3 class="card-title"><strong> Listado de Paciente</strong></h3>
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="table-responsive">
-                                                <table
-                                                    class="table table-hover js-basic-example dataTable table_custom border-style spacing5">
-                                                    <thead>
-                                                    <tr>
-                                                        <th><strong>#</strong></th>
-                                                        <th><strong>Factura</strong></th>
-                                                        <th><strong>Paciente</strong></th>
-                                                        <th><strong>Prioridad</strong></th>
-                                                        <th><strong>Fecha Admisión</strong></th>
-                                                        <th><strong>Tiempo Atención</strong></th>
-                                                        <th><strong>Técnico</strong></th>
-                                                        <th><strong>Profesional</strong></th>
-                                                    </tr>
-                                                    </thead>
-                                                    <tfoot>
-                                                    <tr>
-                                                        <th><strong>#</strong></th>
-                                                        <th><strong>Factura</strong></th>
-                                                        <th><strong>Paciente</strong></th>
-                                                        <th><strong>Prioridad</strong></th>
-                                                        <th><strong>Fecha Admisión</strong></th>
-                                                        <th><strong>Tiempo Atención</strong></th>
-                                                        <th><strong>Técnico</strong></th>
-                                                        <th><strong>Profesional</strong></th>
-                                                    </tr>
-                                                    </tfoot>
-                                                    <tbody>
-                                                        @foreach($data as $a)
-                                                            <tr>
-                                                                    <td>{{$loop->index+1}}</td>
-                                                                    <td>{{$a->admission->doctype}}-{{$a->admission->invoice_number}}</td>
-                                                                    <td><span>{{$a->admission->patient->name}}</span></td>
-                                                                    <td><span class="tag tag-default">{{$a->admission->priority}}</span></td>
-                                                                    <td><span>{{$a->admission_date}}</span></td>
-                                                                    <td><span>{{$a->attention_time}}</span></td>
-                                                                    <td><span>{{$a->user->name}}</span></td>
-                                                                    <td>{{$a->admission->user->name}}</td>
-                                                            </tr>
-                                                        @endforeach
-                                                        </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
+        <div class="section-body mt-3">
+            <div class="container-fluid">
+                <div class="row clearfix">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title"><strong> Listado de Paciente</strong></h3>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table
+                                        class="table table-hover js-basic-example dataTable table_custom border-style spacing5">
+                                        <thead>
+                                        <tr>
+                                            <th><strong>#</strong></th>
+                                            <th><strong>Factura</strong></th>
+                                            <th><strong>Identificación Paciente</strong></th>
+                                            <th><strong>Paciente</strong></th>
+                                            <th><strong>Prioridad</strong></th>
+                                            <th><strong>Fecha Admisión</strong></th>
+                                            <th><strong>Tiempo Atención</strong></th>
+                                            <th><strong>Num Ordenes Serv</strong></th>
+                                            <th><strong>Profesional</strong></th>
+                                        </tr>
+                                        </thead>
+                                        <tfoot>
+                                        <tr>
+                                            <th><strong>#</strong></th>
+                                            <th><strong>Factura</strong></th>
+                                            <th><strong>Identificación Paciente</strong></th>
+                                            <th><strong>Paciente</strong></th>
+                                            <th><strong>Prioridad</strong></th>
+                                            <th><strong>Fecha Admisión</strong></th>
+                                            <th><strong>Tiempo Atención</strong></th>
+                                            <th><strong>Num Ordenes Serv</strong></th>
+                                            <th><strong>Profesional</strong></th>
+                                        </tr>
+                                        </tfoot>
+                                        <tbody>
+                                           
+                                            @foreach($data as $a)
+                                                <tr>
+                                                        <td>{{$loop->index+1}}</td>
+                                                        <td>{{$a->admission->doctype}}-{{$a->admission->invoice_number}}</td>
+                                                        <td><span>{{$a->admission->patient->legal_id}}</span></td>
+                                                        <td><span>{{$a->admission->patient->name}}</span></td>
+                                                        <td><span class="tag tag-default">{{$a->admission->priority}}</span></td>
+                                                        <td><span>{{$a->admission_date}}</span></td>
+                                                        <td><span>{{$a->attention_time}}</span></td>
+                                                        <td><span>{{$a->order_count}}</span></td>
+                                                        <td>{{$a->admission->user->name}}</td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-                @endisset
+                </div>
+            </div>
+        </div>
+    @endisset
 
 @endsection
 @section('before-scripts')
